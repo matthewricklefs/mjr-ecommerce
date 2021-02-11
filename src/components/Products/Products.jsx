e1ef788f1a168aeb3ff1c5614823335a6b65a594
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-02-11 11:36:30 -0500

    creates src folder, creates components, the Products and Product component and styles for the MaterialUI cards

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product";
+
+const products = [
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running Shoes",
+    price: "$5",
+    image:
+      "https://images.unsplash.com/photo-1523314399-837f3e0c291b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHJ1bm5pbmclMjBzaG9lc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 2,
+    name: "MacBook",
+    description: "Apple MacBook",
+    price: "$10",
+    image:
+      "https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1957&q=80",
+  },
+];
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} s={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
