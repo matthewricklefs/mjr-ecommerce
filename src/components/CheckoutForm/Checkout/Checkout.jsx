36053bd78a4c8dabaa64051447dfab18346eeeee
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-02-13 10:49:52 -0500

    finalizes the paymentform and checkout components, updates sandbox key for commerce.js

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -15,7 +15,7 @@ import AddressForm from "../AddressForm";
 import PaymentForm from "../PaymentForm";
 
 const steps = ["Shipping Address", "Payment Details"];
-const Checkout = ({ cart }) => {
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
   const [shippingData, setShippingData] = useState({});
@@ -55,6 +55,7 @@ const Checkout = ({ cart }) => {
         shippingData={shippingData}
         checkoutToken={checkoutToken}
         backStep={backStep}
+        onCaptureCheckout={onCaptureCheckout}
       />
     );
 
